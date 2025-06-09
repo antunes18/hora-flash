5a6985f9f1d6f16f60a6d74a3f568745e75545fe
Xandy <alexandrearthurpg@gmail.com>
2025-06-09 19:16:42 -0300

    fix: add __init__.py to test modules

--- api/core/database.py
+++ api/core/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQL_ALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+SQL_ALCHEMY_DATABASE_URL = os.getenv("DB_URL", "sqlite+aiosqlite:///./default.db")
 
 
 engine = create_engine(
