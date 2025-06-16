055a42d71026ec0e6254378f5b28a892bd4ee9dd
Xandy <alexandrearthurpg@gmail.com>
2025-06-16 16:24:44 -0300

    merge: resolve merge conflicts

--- test/mocks/user.py
+++ test/mocks/user.py
@@ -1,4 +1,5 @@
 import pytest
+<<<<<<< HEAD
 from sqlalchemy.ext.asyncio import AsyncSession
 from api.models.user import User
 from api.models.dto.user_dto import UserCreateDTO, UserLoginDTO, UserResponseDTO
@@ -29,13 +30,23 @@ def user_repository(db_session_for_test: AsyncSession):
 def user_service_real_repo(user_repository: UserRepository):
     """Fixture que fornece uma instância do UserService com um repositório real (para testes de integração)."""
     return UserServices(user_repo=user_repository)
+=======
+from api.models.user import User
+from api.models.dto.user_dto import UserCreateDTO, UserLoginDTO, UserResponseDTO
+from api.models.enums.roles import Roles
+>>>>>>> 2ecb84c (fix: resolve conflict)
 
 
 @pytest.fixture(scope="function")
 def test_user_create():
     return UserCreateDTO(
+<<<<<<< HEAD
         username="teste_de_user",
         email="teste@teste.com",
+=======
+        username="Teste",
+        email="email@email.com",
+>>>>>>> 2ecb84c (fix: resolve conflict)
         password="stringstri",
         confirm_password="stringstri",
         role=Roles.user,
@@ -47,6 +58,7 @@ def test_user_login():
     return UserLoginDTO(email="email@email.com", password="stringstri")
 
 
+<<<<<<< HEAD
 @pytest.fixture
 def mock_user():
     return User(
@@ -108,3 +120,12 @@ def mock_list_user():
             disabled=False,
         ),
     ]
+=======
+mock_user_create = User(
+    username="Teste",
+    email="email@email.com",
+    password="stringstri",
+    role="user",
+    disabled=False,
+)
+>>>>>>> 2ecb84c (fix: resolve conflict)
--- test/services/test_user_services.py
+++ test/services/test_user_services.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from logging import disable
 from unittest.mock import AsyncMock
 from fastapi import responses
@@ -137,3 +138,22 @@ class TestUserServices:
 
         assert response is not None
         assert response.disabled is False
+=======
+from fastapi import responses
+from api.models.dto.user_dto import UserCreateDTO, UserLoginDTO
+from api.models.enums.roles import Roles
+from api.services import auth_services as services
+import pytest
+
+from test.conftest import auth_headers, client
+from test.mocks.user import mock_user_create, test_user_create, test_user_login
+
+
+class TestUserServices:
+    def test_register_user(self, test_user_create, db_session):
+        response = services.register_user(test_user_create, db_session)
+        assert mock_user_create.email == response.email
+        assert mock_user_create.username == response.username
+        assert mock_user_create.role == response.role
+        assert mock_user_create.disabled == response.disabled
+>>>>>>> 2ecb84c (fix: resolve conflict)
